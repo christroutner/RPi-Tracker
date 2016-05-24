d1e2e3103f4225d64faec59ee2f1e35f9d734032
Chris Troutner <chris.troutner@gmail.com>
2016-05-23 18:12:55 -0700

    Adding Convertion from Degrees,minutes,seconds to decimal degrees.

--- app.js
+++ app.js
@@ -173,6 +173,7 @@ app.use('/send_email', function(request, response, next) {
 /*
  * Utility function for converting between Degrees, Minutes Seconds (Raw GPS output) and Decimal Degree (Google Maps) format.
  */
+/*
 var ConvertDMSToDD = function(degrees, minutes, seconds, direction) {
     var dd = degrees + minutes/60 + seconds/(60*60);
 
@@ -181,7 +182,7 @@ var ConvertDMSToDD = function(degrees, minutes, seconds, direction) {
     } // Don't do anything for N or E
     return dd;
 }
-
+*/
 
 /*
  * GPS Connection

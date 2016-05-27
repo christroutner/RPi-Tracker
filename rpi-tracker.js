720bd64161ee0bce4c597b0d9a60feb0beed6b69
Chris Troutner <chris.troutner@gmail.com>
2016-05-27 14:40:45 -0700

    Creating UI

--- rpi-tracker.js
+++ rpi-tracker.js
@@ -216,6 +216,8 @@ var listener = new gpsd.Listener({
 });
 
 listener.connect(function() {  
+  //Dev Note: This message gets displayed weather or not the device was actually able to connect to the GPS. I need a way to write out to the console
+  //weather the GPS was successful or not.
   console.log('Connected to GPS');
 });
 

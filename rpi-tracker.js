64b372e74f1d83cfad863cb22486b66c7319006f
Chris Troutner <chris.troutner@gmail.com>
2016-09-20 19:13:05 -0700

    Creating GPS interface library

--- rpi-tracker.js
+++ rpi-tracker.js
@@ -56,48 +56,10 @@ var docDesc = "Tracking data captured with the Raspberry Pi on "+today.getFullYe
 
 //Read in the log files if they already exists. Otherwise create a new file.
 //First log file.
-fs.readFile('./data/'+fileNameGeoJSONPoint, 'utf8', function(err, data) {
-  if (err) {
-    //debugger;
-    
-    //The file doesn't exist, so create the GeoJSON structure from scratch.
-    if( err.code == "ENOENT" ) {    
-      
-      jsonPointTimeStamp.data = 
-      {
-        "type": "FeatureCollection",
-          "features": 
-        [            
-          { 
-            "type": "Feature",
-            "geometry": {"type": "Point", "coordinates": [-122.36459, 48.30898]},
-            "properties": 
-            {
-              "timestamp": "",
-              "name": ""
-            }
-          }
-        ]
-      };
-  
-      //Set flags for file handling.
-      jsonPointTimeStamp.fileRead = true;
-      jsonPointTimeStamp.exists = false;
-      
-    //Handle unknown errors.
-    } else {
-      console.log('Error opening the JSON Point file.');
-      throw err;
-    }
-    
-  } else {
-    //debugger;
-    //If the file already exists, the read it in.
-    jsonPointTimeStamp.data = JSON.parse(data);
-    jsonPointTimeStamp.fileRead = true;
-    jsonPointTimeStamp.exists = true;
-  }
-});
+dataLog.readPointFile();
+
+
+
 
 //Second log file.
 fs.readFile('./data/'+fileNameGeoJSONLineString, 'utf8', function(err, data) {

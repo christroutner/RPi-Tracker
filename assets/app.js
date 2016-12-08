88b06a49859bcd48c2327cb27dd28cfd0c7745f1
Chris Troutner <chris.troutner@gmail.com>
2016-12-07 19:35:06 -0800

    Developing API to update RPi-Tracker software on button click

--- assets/app.js
+++ assets/app.js
@@ -489,9 +489,9 @@ $(document).ready(function() {
   
   //This function is called when the user clicks the 'Update RPi-Tracker Software' button.
   //It makes an API call that initializes a 'git pull' from the RPi-Tracker GitHub repo and then a reboot.
-  function updateSoftware() {
+  $('#updateSoftwareBtn').click(function(event) {
     debugger;
-  }
+  });
   
   
   // END SETTINGS TAB CONTROL

4a8ea63d6f86ee5ea3fb974c07fc6d7490ef4436
Chris Troutner <chris.troutner@gmail.com>
2017-05-04 07:41:56 -0700

    Stopping the deletion of log files on bootup.

--- rpi-tracker.js
+++ rpi-tracker.js
@@ -44,8 +44,8 @@ global.appLogAPI = new AppLogAPI.Constructor();
 //dataLog.helloWorld();
 
 //Clear the PM2 log before starting anything else.
-if(!global.appLogAPI.clearLog())
-  console.log('Error trying to clear the PM2 log!');
+//if(!global.appLogAPI.clearLog())
+//  console.log('Error trying to clear the PM2 log!');
 
 
 /*
